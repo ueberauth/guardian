a9c4598e4e1b5b592886cf3ebe31c8c2a1336d3c
Daniel Neighman <has.sox@gmail.com>
2015-06-18 18:30:19 -0700

    Update the channel interface

--- lib/guardian/channel.ex
+++ lib/guardian/channel.ex
@@ -1,36 +1,72 @@
 defmodule Guardian.Channel do
+  @moduledoc """
+  Provides integration for channels to use Guardian tokens.
+
+  ## Example
+
+      defmodule MyApp.MyChannel do
+        use Phoenix.Channel
+        use Guardian.Channel
+
+        def join(_room, %{ claims: claims, resource: resource }, socket) do
+          { :ok, %{ message: "Joined" }, socket }
+        end
+
+        def join(room, _, socket) do
+          { :error,  :authentication_required }
+        end
+
+        def handle_in("ping", _payload, socket) do
+          user = Guardian.Channel.current_resource(socket)
+          broadcast socket, "pong", %{ message: "pong", from: user.email }
+          { :noreply, socket }
+        end
+      end
+
+  Tokens will be parsed and the claims and resource assigned to the socket.
+
+  When using `:csrf` token types, you need to pass the csrf token up with the join message.
+
+  ## Example
+
+      let socket = new Socket("/ws");
+      socket.connect();
+
+      let guardianToken = jQuery('meta[name="guardian_token"]').attr('content');
+      let csrfToken = jQuery('meta[name="csrf_token"]').attr('content');
+
+      let chan = socket.chan("pings", { guardian_token: guardianToken, csrf_token: csrfToken });
+  """
   defmacro __using__(opts) do
+    key = Dict.get(opts, :key, :default)
+
     quote do
       def join(room, auth = %{ "guardian_token" => jwt, "csrf_token" => csrf_token }, socket) do
-        Guardian.Channel.handle_join(__MODULE__, room, jwt, %{ csrf: csrf_token }, socket, unquote(opts))
+        handle_guardian_join(room, jwt, %{ csrf: csrf_token }, socket)
       end
 
       def join(room, auth = %{ "guardian_token" => jwt }, socket) do
-        Guardian.Channel.handle_join(__MODULE__, room, jwt, %{ }, socket, unquote(opts))
+        handle_guardian_join(room, jwt, %{ }, socket)
       end
-    end
-  end
 
-  def handle_error(reason, opts) do
-    case Dict.get(opts, :on_failure) do
-      { mod, method } -> apply(mod, method, [reason])
-      _ -> { :error, %{ reason: reason } }
-    end
-  end
+      def handle_guardian_auth_failure(reason), do: { :error, %{ error: reason } }
 
-  def handle_join(mod, room, jwt, params, socket, opts) do
-    key = Dict.get(opts, :key, :default)
-    case Guardian.verify(jwt, params) do
-      { :ok, claims } ->
-        case Guardian.serializer.from_token(Dict.get(claims, :sub)) do
-          { :ok, resource } ->
-            authed_socket = socket
-            |> Phoenix.Channel.assign(Guardian.Keys.claims_key(key), claims)
-            |> Phoenix.Channel.assign(Guardian.Keys.resource_key(key), resource)
-            apply(mod, :join, [room, %{ guardian: claims }, authed_socket])
-          { :error, reason } -> handle_error(reason, opts)
+      defp handle_guardian_join(room, jwt, params, socket) do
+        case Guardian.verify(jwt, params) do
+          { :ok, claims } ->
+            case Guardian.serializer.from_token(Dict.get(claims, :sub)) do
+              { :ok, resource } ->
+                authed_socket = socket
+                |> assign(Guardian.Keys.claims_key(unquote(key)), claims)
+                |> assign(Guardian.Keys.resource_key(unquote(key)), resource)
+                join(room, %{ claims: claims, resource: resource }, authed_socket)
+              { :error, reason } -> handle_guardian_auth_failure(reason)
+            end
+          { :error, reason } -> handle_guardian_auth_failure(reason)
         end
-      { :error, reason } -> handle_error(reason, opts)
+      end
+
+      defoverridable [handle_guardian_auth_failure: 1]
     end
   end
 
