a7687d060016359a054aef1f82a2151f849a21ba
Andrea Azzini <and.azzini@gmail.com>
2015-08-26 17:30:40 +0200

    Remove unnecessary comments

--- mix.exs
+++ mix.exs
@@ -19,22 +19,10 @@ defmodule Guardian.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :joken, :poison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:joken, "~> 0.15.0"},
      {:poison, "~> 1.5"},
