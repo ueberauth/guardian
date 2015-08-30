353794244503dab2853e2982f6cbb7838084484f
Daniel Neighman <has.sox@gmail.com>
2015-08-30 15:16:10 -0700

    Update docs

--- doc/dist/sidebar_items.js
+++ doc/dist/sidebar_items.js
@@ -1,22 +1,22 @@
 sidebarNodes = {
   "modules": [
     {"id": "Guardian",
-"docs": ["issuer/0","mint/1","mint/2","mint/3","revoke!/1","revoke!/2","serializer/0","verify/1","verify/2","verify!/1","verify!/2"]
+"docs": ["decode_and_verify/1","decode_and_verify/2","decode_and_verify!/1","decode_and_verify!/2","encode_and_sign/1","encode_and_sign/2","encode_and_sign/3","issuer/0","revoke!/1","revoke!/2","serializer/0"]
 },{"id": "Guardian.Channel",
 "docs": ["claims/2","current_resource/2"]
 },{"id": "Guardian.Hooks",
-"docs": ["before_mint/3","after_mint/4","after_sign_in/2","before_sign_out/2","on_verify/2","on_revoke/2"]
+"docs": ["before_encode_and_sign/3","after_encode_and_sign/4","after_sign_in/2","before_sign_out/2","on_verify/2","on_revoke/2"]
 },{"id": "Guardian.Hooks.Default",
-"docs": ["after_mint/4","after_sign_in/2","before_mint/3","before_sign_out/2","on_revoke/2","on_verify/2"]
+"docs": ["after_encode_and_sign/4","after_sign_in/2","before_encode_and_sign/3","before_sign_out/2","on_revoke/2","on_verify/2"]
 },{"id": "Guardian.Permissions",
 "docs": ["all?/3","any?/3","available/0","available/1","from_claims/1","max/0","to_list/1","to_list/2","to_list/3","to_value/1"]
 },{"id": "Guardian.Plug",
 "docs": ["claims/2","current_resource/2","current_token/2","sign_in/2","sign_in/3","sign_in/4","sign_out/2"]
+},{"id": "Guardian.Plug.EnsureAuthenticated"
 },{"id": "Guardian.Plug.EnsurePermissions",
 "docs": ["init/1"]
-},{"id": "Guardian.Plug.EnsureSession"
 },{"id": "Guardian.Plug.LoadResource"
-},{"id": "Guardian.Plug.VerifyAuthorization",
+},{"id": "Guardian.Plug.VerifyHeader",
 "docs": ["call/2","init/1"]
 },{"id": "Guardian.Plug.VerifySession"
 },{"id": "Guardian.Serializer",
