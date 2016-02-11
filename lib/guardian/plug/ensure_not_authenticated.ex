2dea977853e56016087713630b3e74fb870e4525
Victor campos <victor.campos@visagio.com>
2016-02-10 22:51:36 -0200

    adding ensure_not_authenticated plug

---
+++ lib/guardian/plug/ensure_not_authenticated.ex
@@ -0,0 +1,85 @@
+defmodule Guardian.Plug.EnsureNotAuthenticated do
+  @moduledoc """
+  This plug ensures that a valid JWT was provided and has been
+  verified on the request.
+
+  If one is not found, the `authenticated/2` function is invoked with the
+  `Plug.Conn.t` object and its params.
+
+  ## Example
+
+      # Will call the authenticated/2 function on your handler
+      plug Guardian.Plug.EnsureAuthenticated, handler: SomeModule
+
+      # look in the :secret location.  You can also do simple claim checks:
+      plug Guardian.Plug.EnsureAuthenticated, handler: SomeModule, key: :secret
+
+      plug Guardian.Plug.EnsureAuthenticated, handler: SomeModule, aud: "token"
+
+  If the handler option is not passed, `Guardian.Plug.ErrorHandler` will provide
+  the default behavior.
+  """
+  require Logger
+  import Plug.Conn
+
+  @doc false
+  def init(opts) do
+    opts = Enum.into(opts, %{})
+    handler = build_handler_tuple(opts)
+
+    claims_to_check = Map.drop(opts, [:on_failure, :key, :handler])
+    %{
+      handler: handler,
+      key: Map.get(opts, :key, :default),
+      claims: Guardian.Utils.stringify_keys(claims_to_check)
+    }
+  end
+
+  @doc false
+  def call(conn, opts) do
+    key = Map.get(opts, :key, :default)
+
+    case Guardian.Plug.claims(conn, key) do
+      { :ok, claims } -> conn |> check_claims(opts, claims)
+      { :error, reason } -> conn
+      _ -> conn
+    end
+  end
+
+  @doc false
+  defp handle_error(conn, reason, opts) do
+    the_connection = conn |> assign(:guardian_failure, reason) |> halt
+
+    {mod, meth} = Map.get(opts, :handler)
+    apply(
+      mod,
+      meth,
+      [the_connection, Map.merge(the_connection.params, %{ reason: reason })]
+    )
+  end
+
+  defp check_claims(conn, opts = %{ claims: claims_to_check }, claims) do
+    claims_match = claims_to_check
+                   |> Map.keys
+                   |> Enum.all?(&(claims_to_check[&1] == claims[&1]))
+    if claims_match do
+      handle_error(conn, { :error, :claims_match }, opts)
+    else
+      conn
+    end
+  end
+
+  defp build_handler_tuple(%{handler: mod}) do
+    {mod, :authenticated}
+  end
+  defp build_handler_tuple(%{on_failure: {mod, func}}) do
+    Logger.log(
+      :warn,
+      ":on_failure is deprecated. Use the :handler option instead"
+    )
+    {mod, func}
+  end
+  defp build_handler_tuple(_) do
+    {Guardian.Plug.ErrorHandler, :authenticated}
+  end
+end
---
+++ test/guardian/plug/ensure_not_authenticated_test.exs
@@ -0,0 +1,115 @@
+defmodule Guardian.Plug.EnsureNotAuthenticatedTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias Guardian.Plug.EnsureNotAuthenticated
+
+  defmodule TestHandler do
+    def authenticated(conn, _) do
+      conn
+      |> Plug.Conn.assign(:guardian_spec, :authenticated)
+      |> Plug.Conn.send_resp(401, "Authenticated")
+    end
+  end
+
+  test "init/1 sets the handler option to the module that's passed in" do
+    %{handler: handler_opts} = EnsureNotAuthenticated.init(handler: TestHandler)
+
+    assert handler_opts == {TestHandler, :authenticated}
+  end
+
+  test "init/1 sets the handler option to the value of on_failure" do
+    %{handler: handler_opts} = EnsureNotAuthenticated.init(
+      on_failure: {TestHandler, :custom_failure_method}
+    )
+
+    assert handler_opts == {TestHandler, :custom_failure_method}
+  end
+
+  test "init/1 defaults the handler option to Guardian.Plug.ErrorHandler" do
+    %{handler: handler_opts} = EnsureNotAuthenticated.init %{}
+
+    assert handler_opts == {Guardian.Plug.ErrorHandler, :authenticated}
+  end
+
+  test "init/1 with default options" do
+    options = EnsureNotAuthenticated.init %{}
+
+    assert options == %{
+      claims: %{},
+      handler: {Guardian.Plug.ErrorHandler, :authenticated},
+      key: :default
+    }
+  end
+
+  test "init/1 uses all opts as claims except :on_failure, :key and :handler" do
+    %{claims: claims} = EnsureNotAuthenticated.init(
+      on_failure: {TestHandler, :some_method},
+      key: :super_secret,
+      handler: TestHandler,
+      foo: "bar",
+      another: "option"
+    )
+
+    assert claims == %{"foo" => "bar", "another" => "option"}
+  end
+
+  test "it validates claims and calls through if the claims are ok" do
+    claims = %{ "aud" => "token", "sub" => "user1" }
+    conn = :get |> conn("/foo") |> Guardian.Plug.set_claims({ :ok, claims })
+    opts = EnsureNotAuthenticated.init(handler: TestHandler, aud: "token")
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    assert must_authenticate?(ensured_conn)
+  end
+
+  test "it validates claims and fails if the claims do not match" do
+    claims = %{ "aud" => "oauth", "sub" => "user1" }
+    conn = :get |> conn("/foo") |> Guardian.Plug.set_claims({:ok, claims})
+    opts = EnsureNotAuthenticated.init(handler: TestHandler, aud: "token")
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    refute must_authenticate?(ensured_conn)
+  end
+
+  test "doesn't call unauthenticated when there's a session with default key" do
+    claims = %{ "aud" => "token", "sub" => "user1" }
+    conn = :get |> conn("/foo") |> Guardian.Plug.set_claims({ :ok, claims })
+    opts = EnsureNotAuthenticated.init(handler: TestHandler)
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    assert must_authenticate?(ensured_conn)
+  end
+
+  test "doesn't call unauthenticated when theres a session with specific key" do
+    claims = %{ "aud" => "token", "sub" => "user1" }
+    conn = :get
+            |> conn("/foo")
+            |> Guardian.Plug.set_claims({:ok, claims}, :secret)
+    opts = EnsureNotAuthenticated.init(handler: TestHandler, key: :secret)
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    assert must_authenticate?(ensured_conn)
+  end
+
+  test "calls handler's unauthenticated/2 with no session for default key" do
+    conn = conn(:get, "/foo")
+    opts = EnsureNotAuthenticated.init(handler: TestHandler)
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    refute must_authenticate?(ensured_conn)
+  end
+
+  test "calls handler's unauthenticated/2 with no session for specific key" do
+    conn = conn(:get, "/foo")
+    opts = EnsureNotAuthenticated.init(handler: TestHandler, key: :secret)
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    refute must_authenticate?(ensured_conn)
+  end
+
+  test "it halts the connection" do
+    conn = conn(:get, "/foo")
+    opts = EnsureNotAuthenticated.init(handler: TestHandler, key: :secret)
+    ensured_conn = EnsureNotAuthenticated.call(conn, opts)
+    refute ensured_conn.halted
+  end
+
+  defp must_authenticate?(conn) do
+    conn.assigns[:guardian_spec] == :authenticated
+  end
+end
