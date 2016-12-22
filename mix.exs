4dde221259f0c9eb18896c78c9fb154bf6665ded
Daniel Neighman <has.sox@gmail.com>
2016-12-22 13:55:25 -0800

    Bump versions for release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Guardian.Mixfile do
   use Mix.Project
 
-  @version "0.13.1"
+  @version "0.14.0"
   @url "https://github.com/ueberauth/guardian"
   @maintainers [
     "Daniel Neighman",
