1e0dad1e92738ce5314b6510d3a829d3c59a291d
Daniel Neighman <has.sox@gmail.com>
2017-05-19 17:55:14 -0700

    Working encode and decode. Not yet specs

--- lib/guardian.ex
+++ lib/guardian.ex
@@ -1,97 +1,173 @@
 defmodule Guardian do
   @type token_type :: String.t
+  @type options :: Keyword.t
+
+  @default_token_module Guardian.Token.Jwt
+  @default_token_type "access"
 
   @doc """
   Encodes the subject into a token in the "sub" field
   """
   @callback subject_for_token(
-    resource :: any,
-    claims :: map
-  ) :: string
+    resource :: Guardian.Token.resource(),
+    claims :: Guardian.Token.claims()
+  ) :: {:ok, String.t} | {:error, atom()}
 
   @callback resource_from_claims(
-    claims :: map
-  ) :: any
+    claims :: Guardian.Token.claims()
+  ) :: {:ok, Guardian.Token.resource()} | {:error, atom()}
 
-  @callback token_module() :: Module.t
-  @callback config() :: map
+  @callback build_claims(
+    claims :: Guardian.Token.claims(),
+    resource :: Guardian.Token.resource(),
+    opts :: options()
+  ) :: {:ok, Guardian.Token.claims()} | {:error, atom()}
 
-  @callback encode_and_sign(
+  @callback after_encode_and_sign(
     resource :: any,
-    type :: token_type,
-    claims :: map,
-    options :: Keyword.t
-  ) :: {:ok, Token.jwt, map} |
-       Token.signing_error |
-       Token.encoding_error
+    claims :: Guardian.Token.claims(),
+    token :: Guardian.Token.token(),
+    options :: options()
+  ) :: {:ok, Guardian.Token.token()} | {:error, atom()}
 
-  @callback decode_and_verify(
-    token :: Token.jwt,
-    claims_to_check :: map,
-    options :: Keyword.t
-  ) :: {:ok, map} |
-       {:error, atom}
+  @callback after_sign_in(
+    conn :: Plug.Conn.t,
+    location :: atom() | nil
+  ) :: Plug.Conn.t
 
-  defmacro __using__(opts) do
-    opts = Enum.into(opts, %{})
-    otp_app = Map.get(opts, :otp_app)
-    token_mod = Map.get(opts, :token_module)
+  @callback before_sign_out(
+    conn :: Plug.Conn.t,
+    location :: atom() | nil
+  ) :: Plug.Conn.t
+
+  @callback verify_claims(
+    claims :: Guardian.Token.claims(),
+    claims_to_check :: Guardian.Token.claims(),
+    options :: options()
+  ) :: {:ok, Guardian.Token.claims()} | {:error, atom()}
+
+  @callback on_verify(
+    claims :: Guardian.Token.claims(),
+    token :: Guardian.Token.token(),
+    options :: options()
+  ) :: {:ok, Guardian.Token.claims()} | {:error, any()}
+
+  @callback on_revoke(
+    claims :: Guardian.Token.claims(),
+    token :: Guardian.Token.token(),
+    options :: options()
+  ) :: {:ok, Guardian.Token.claims()} | {:error, any()}
+
+  defmacro __using__(opts \\ []) do
+    otp_app = Keyword.get(opts, :otp_app)
 
     quote do
       @behaviour Guardian
+      Guardian.Config.merge_config_options(__MODULE__, unquote(opts))
+
+      def default_token_type, do: "access"
 
       def config do
         Application.get_env(unquote(otp_app), __MODULE__)
       end
 
-      def token_module do
-        unquote(token_mod) ||
-          Map.get(config, :token_module, Guardian.Token.Jwt)
+      def config(key, default \\ nil) do
+        config()
+        |> Keyword.get(key, default)
+        |> Guardian.Config.resolve_value()
       end
 
-      def encode_and_sign(resource, token_type, claims \\ [], opts \\ [])
+      def encode_and_sign(resource, token_type \\ nil, claims \\ %{}, opts \\ []) do
+        token_type = token_type || default_token_type()
         Guardian.encode_and_sign(__MODULE__, resource, token_type, claims, opts)
       end
 
-      def decode_and_verify(token, claims_to_check \\ %{}, opts \\ [])
+      def decode_and_verify(token, claims_to_check \\ %{}, opts \\ []) do
         Guardian.decode_and_verify(__MODULE__, token, claims_to_check, opts)
       end
 
-      @defoverridable [config: 0, token_module: 0, secret: 2]
+      def after_encode_and_sign(_r, _claims, token, _), do: {:ok, token}
+      def after_sign_in(conn, _location), do: conn
+      def before_sign_out(conn, _location), do: conn
+      def on_verify(claims, _token, _options), do: {:ok, claims}
+      def on_revoke(claims, _token, _options), do: {:ok, claims}
+
+      def build_claims(c, _, _), do: {:ok, c}
+      def verify_claims(claims, _claims_to_check, _options), do: {:ok, claims}
+
+      @defoverridable [
+        after_encode_and_sign: 5,
+        after_sign_in: 2,
+        before_sign_out: 2,
+        build_claims: 1,
+        default_token_type: 0,
+        on_revoke: 3,
+        on_verify: 3,
+        verify_claims: 3,
+      ]
     end
   end
 
+  def timestamp do
+    System.system_time(:seconds)
+  end
+
+  def stringify_keys(map) do
+    for {k,v} <- map, into: %{}, do: {to_string(k), v}
+  end
+
   def encode_and_sign(mod, resource, token_type, claims \\ %{}, opts \\ []) do
-    claims = Enum.into(claims, %{})
-    token_mod = apply(mod, :token_module, [])
-    sub = apply(mod, :subject_for_token, [resource, claims])
+    claims =
+      claims
+      |> Enum.into(%{})
+      |> Guardian.stringify_keys()
 
-    case sub do
-      {:ok, subject} ->
-        claims = stringify_keys(claims)
-        full_claims = apply(
-          token_mod,
-          :build_claims,
-          [mod, subject, token_type, claims, opts]
-        )
+    token_mod = apply(mod, :config, [:token_module, @default_token_module])
 
-        case full_claims do
-          {:ok, claims} ->
-            apply(token_mod, :sign_claims, [mod, claims, opts])
-          {:error, reason} -> {:error, reason}
-        end
-      {:error, reason} -> {:error, reason}
+    with {:ok, subject} <- apply(mod, :subject_for_token, [resource, claims]),
+         claims <- apply(
+           token_mod,
+           :build_claims,
+           [mod, resource, subject, token_type, claims, opts]
+         ),
+         {:ok, claims} <- apply(mod, :build_claims, [claims, resource, opts]),
+         {:ok, token} <- apply(token_mod, :create_token, [mod, claims, opts]),
+         {:ok, _} <- apply(
+           mod,
+           :after_encode_and_sign,
+           [resource, claims, token, opts]
+         )
+    do
+      {:ok, token, claims}
     end
   end
 
   def decode_and_verify(mod, token, claims_to_check \\ %{}, opts \\ []) do
-    claims_to_check = Enum.into(claims_to_check, %{})
-    token_mod = apply(mod, :token_module, [])
-    decode_result = apply(token_mod, :decode_token, [mod, token, opts])
+    claims_to_check =
+      claims_to_check
+      |> Enum.into(%{})
+      |> Guardian.stringify_keys()
 
-    case decode_result do
-      {:ok, jwt}
+    token_mod = apply(mod, :config, [:token_module, @default_token_module])
+
+    try do
+      with {:ok, claims} <- apply(token_mod, :decode_token, [mod, token, opts]),
+           {:ok, claims} <- apply(
+             token_mod,
+             :verify_claims,
+             [mod, claims, claims_to_check, opts]
+           ),
+           {:ok, claims} <- apply(
+             mod,
+             :verify_claims,
+             [claims, claims_to_check, opts]
+           ),
+           {:ok, claims} <- apply(mod, :on_verify, [claims, token, opts])
+      do
+        {:ok, claims}
+      end
+    rescue
+      e -> {:error, e}
     end
-
   end
 end
--- lib/guardian/signature/config.ex
+++
@@ -1,6 +0,0 @@
-defmodule Guardian.Signature.Config do
-  @moduledoc """
-  Configuration for verifying a signature
-  """
-  defstruct allowed_algos: ["HS512"], allowed_drift: 1_000, secret: nil
-end
