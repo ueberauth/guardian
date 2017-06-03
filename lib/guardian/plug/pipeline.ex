b10dcedd3761c1280d1ec672864ef8287ff6d5e8
Daniel Neighman <has.sox@gmail.com>
2017-06-03 13:23:28 -0700

    Add phoenix helpers (#339)

--- lib/guardian/plug/pipeline.ex
+++ lib/guardian/plug/pipeline.ex
@@ -1,241 +1,243 @@
-defmodule Guardian.Plug.Pipeline do
-  @moduledoc """
-  Helps to build plug pipelines for use with Guardian and associated plugs.
+if Code.ensure_loaded?(Plug) do
+  defmodule Guardian.Plug.Pipeline do
+    @moduledoc """
+    Helps to build plug pipelines for use with Guardian and associated plugs.
 
-  All Guardian provided plugs have a number of features.
+    All Guardian provided plugs have a number of features.
 
-  1. They take a `:key` option to know where to store information in the session and connection
-  2. They require a reference to the implementation (the module that `use Guardian`)
-  3. They require a reference to an error handling module
+    1. They take a `:key` option to know where to store information in the session and connection
+    2. They require a reference to the implementation (the module that `use Guardian`)
+    3. They require a reference to an error handling module
 
-  These references are passed through the connection so they must be put in place
-  before the Guardian Plugs. By using a pipeline this is taken care of for you.
+    These references are passed through the connection so they must be put in place
+    before the Guardian Plugs. By using a pipeline this is taken care of for you.
 
-  The easiest way to use `Guardian.Plug.Pipeline` is to create a module that defines your pipeline.
+    The easiest way to use `Guardian.Plug.Pipeline` is to create a module that defines your pipeline.
 
-  ```elixir
-  defmodule MyApp.AuthPipeline do
-    use Guardian.Plug.Pipeline, otp_app: :my_app,
-                                module: MyApp.Tokens,
-                                error_handler: MyApp.AuthErrorHandler
+    ```elixir
+    defmodule MyApp.AuthPipeline do
+      use Guardian.Plug.Pipeline, otp_app: :my_app,
+                                  module: MyApp.Tokens,
+                                  error_handler: MyApp.AuthErrorHandler
 
-    alias Guardian.Plug.{
-      EnsureAuthenticated,
-      LoadResource,
-      VerifySession,
-      VerifyHeader,
-    }
+      alias Guardian.Plug.{
+        EnsureAuthenticated,
+        LoadResource,
+        VerifySession,
+        VerifyHeader,
+      }
 
-    plug VerifySession, claims: @claims
-    plug VerifyHeader, claims: @claims, realm: "Bearer"
-    plug EnsureAuthenticated
-    plug LoadResource, ensure: true
-  end
-  ```
+      plug VerifySession, claims: @claims
+      plug VerifyHeader, claims: @claims, realm: "Bearer"
+      plug EnsureAuthenticated
+      plug LoadResource, ensure: true
+    end
+    ```
 
-  When you want to use the pipeline you just use it like a normal plug.
+    When you want to use the pipeline you just use it like a normal plug.
 
-  ```elixir
-  plug MyApp.AuthPipeline
-  ```
+    ```elixir
+    plug MyApp.AuthPipeline
+    ```
 
-  This pipeline will look for tokens in either the session (it's ok if it's not loaded)
-  followed by the header if one wasn't found in the session.
+    This pipeline will look for tokens in either the session (it's ok if it's not loaded)
+    followed by the header if one wasn't found in the session.
 
-  We then ensure that we found a token and fail if not.
+    We then ensure that we found a token and fail if not.
 
-  Given that we found a token, we then attempt to load the resource the token
-  refres to, failing if one is not found.
+    Given that we found a token, we then attempt to load the resource the token
+    refres to, failing if one is not found.
 
-  ### Customizing your pipeline
+    ### Customizing your pipeline
 
-  Once you've created a pipeline, you can customize it when you call it with options.
+    Once you've created a pipeline, you can customize it when you call it with options.
 
-  ```elixir
-  plug MyApp.AuthPipeline, module: MyApp.ADifferentGuardianModule
-  # OR
-  plug MyApp.AuthPipeline, key: :impersonate
-  ```
+    ```elixir
+    plug MyApp.AuthPipeline, module: MyApp.ADifferentGuardianModule
+    # OR
+    plug MyApp.AuthPipeline, key: :impersonate
+    ```
 
-  ### Options
+    ### Options
 
-  You can provide options to the pipeline when you `use Guardian.Plug.Pipeline`
-  or you can provide them when you call the plug.
+    You can provide options to the pipeline when you `use Guardian.Plug.Pipeline`
+    or you can provide them when you call the plug.
 
-  Additionally, for every option other than `:otp_app` you can use elixir
-  configuration, the using options, or linline options.
+    Additionally, for every option other than `:otp_app` you can use elixir
+    configuration, the using options, or linline options.
 
-  * `:otp_app` - The otp app where the pipeline modules can be found
-  * `:module` - The `Guardian` implementation module
-  * `:error_handler` - An error handling module. See `Guardian.Plug.Errors`
-  * `:key` - The key to use
+    * `:otp_app` - The otp app where the pipeline modules can be found
+    * `:module` - The `Guardian` implementation module
+    * `:error_handler` - An error handling module. See `Guardian.Plug.Errors`
+    * `:key` - The key to use
 
-  ### Keys
+    ### Keys
 
-  Using keys allows you to specifiy locations in the session/connection where
-  the tokens and resources will be placed. This allows multiple authenticated
-  tokens to be in play for a single request. This is useful for impersonation or
-  higher security areas where you can have a specific set of privilages and
-  still be logged in.
+    Using keys allows you to specifiy locations in the session/connection where
+    the tokens and resources will be placed. This allows multiple authenticated
+    tokens to be in play for a single request. This is useful for impersonation or
+    higher security areas where you can have a specific set of privilages and
+    still be logged in.
 
-  ### Error handler
+    ### Error handler
 
-  When using plugs, you'll need to specify an error handler
+    When using plugs, you'll need to specify an error handler
 
-  The error_handler requires an `auth_error` function that receives the conn
-  and the error reason
+    The error_handler requires an `auth_error` function that receives the conn
+    and the error reason
 
-  ### Inline pipelines
+    ### Inline pipelines
 
-  If you want to define your pipeline inline, you can do so by using
-  `Guardian.Plug.Pipeline` as a plug itself.
-  You _must_ supply the module and error handler inline if you do this.
+    If you want to define your pipeline inline, you can do so by using
+    `Guardian.Plug.Pipeline` as a plug itself.
+    You _must_ supply the module and error handler inline if you do this.
 
-  ```elixir
-  plug Guardian.Plug.Pipeline, module: MyApp.Tokens,
-                          error_handler: MyApp.AuthErrorHandler
-  plug Guardian.VerifyHeader, realm: "Bearer"
-  ```
+    ```elixir
+    plug Guardian.Plug.Pipeline, module: MyApp.Tokens,
+                            error_handler: MyApp.AuthErrorHandler
+    plug Guardian.VerifyHeader, realm: "Bearer"
+    ```
 
-  Inline pipelines are also good to change the error handler that you want to use.
+    Inline pipelines are also good to change the error handler that you want to use.
 
-  Note that you must set the pipeline before using other guardian plugs.
+    Note that you must set the pipeline before using other guardian plugs.
 
-  ```elixir
-  # Use the MyApp.AuthErrorHandler for downstream Guardian plugs
-  plug Guardian.Plug.Pipeline, module: MyApp.Tokens,
-                          error_handler: MyApp.AuthErrorHandler
-  plug Guardian.VerifyHeader, realm: "Bearer"
+    ```elixir
+    # Use the MyApp.AuthErrorHandler for downstream Guardian plugs
+    plug Guardian.Plug.Pipeline, module: MyApp.Tokens,
+                            error_handler: MyApp.AuthErrorHandler
+    plug Guardian.VerifyHeader, realm: "Bearer"
 
-  # Now change out the error handler for plugs downstream of this one.
-  plug Guardian.Plug.Pipeline, error_handler: MyApp.SpecialAuthErrorHandler
-  ```
-  """
+    # Now change out the error handler for plugs downstream of this one.
+    plug Guardian.Plug.Pipeline, error_handler: MyApp.SpecialAuthErrorHandler
+    ```
+    """
 
-  import Plug.Conn
+    import Plug.Conn
 
-  alias Guardian.Plug, as: GPlug
+    alias Guardian.Plug, as: GPlug
 
-  @doc """
-  Create your very own `Guardian.Plug.Pipeline`
+    @doc """
+    Create your very own `Guardian.Plug.Pipeline`
 
-  Using this macro will make your module into a plug builder.
-  It will provide your pipeline with the Guardian implementation module and error
-  handler so that it can be used within your pipeline and downstream.
-  """
-  defmacro __using__(opts \\ []) do
-    alias Guardian.Plug.Pipeline
+    Using this macro will make your module into a plug builder.
+    It will provide your pipeline with the Guardian implementation module and error
+    handler so that it can be used within your pipeline and downstream.
+    """
+    defmacro __using__(opts \\ []) do
+      alias Guardian.Plug.Pipeline
 
-    quote do
-      use Plug.Builder
-      alias Guardian.Config, as: GConfig
-      alias Guardian.Plug, as: GPlug
+      quote do
+        use Plug.Builder
+        alias Guardian.Config, as: GConfig
+        alias Guardian.Plug, as: GPlug
 
-      import Pipeline
+        import Pipeline
 
-      plug :put_modules
+        plug :put_modules
 
-      def init(options) do
-        new_opts =
-          options
-          |> Keyword.merge(unquote(opts))
-          |> config()
+        def init(options) do
+          new_opts =
+            options
+            |> Keyword.merge(unquote(opts))
+            |> config()
 
-        unless Keyword.get(new_opts, :otp_app),
-          do: raise_error(:otp_app)
+          unless Keyword.get(new_opts, :otp_app),
+            do: raise_error(:otp_app)
 
-        unless Keyword.has_key?(new_opts, :module),
-          do: raise_error(:module)
+          unless Keyword.has_key?(new_opts, :module),
+            do: raise_error(:module)
 
-        unless Keyword.has_key?(new_opts, :error_handler),
-          do: raise_error(:error_handler)
+          unless Keyword.has_key?(new_opts, :error_handler),
+            do: raise_error(:error_handler)
 
-        new_opts
-      end
-
-      defp config(opts) do
-        case Keyword.get(opts, :otp_app) do
-          nil -> opts
-          otp_app ->
-            otp_app
-            |> Application.get_env(opt_app, __MODULE__, [])
-            |> Keyword.merge(opts)
+          new_opts
         end
+
+        defp config(opts) do
+          case Keyword.get(opts, :otp_app) do
+            nil -> opts
+            otp_app ->
+              otp_app
+              |> Application.get_env(opt_app, __MODULE__, [])
+              |> Keyword.merge(opts)
+          end
+        end
+
+        defp config(opts, key, default \\ nil) do
+          opts
+          |> config()
+          |> Keyword.get(key)
+          |> GConfig.resolve_value()
+        end
+
+        defp put_modules(conn, opts) do
+          pipeline_opts = [
+            module: config(opts, :module),
+            error_handler: config(opts, :error_handler),
+            key: config(opts, :key, GPlug.default_key()),
+          ]
+
+          Pipeline.call(conn, pipeline_opts)
+        end
+
+        defp raise_error(key),
+          do: raise "Config `#{key}` is missing for #{__MODULE__}"
       end
+    end
 
-      defp config(opts, key, default \\ nil) do
-        opts
-        |> config()
-        |> Keyword.get(key)
-        |> GConfig.resolve_value()
+    def init(opts), do: opts
+
+    def call(conn, opts) do
+      conn
+      |> maybe_put_key(:guardian_module, Keyword.get(opts, :module))
+      |> maybe_put_key(:guardian_error_handler, Keyword.get(opts, :error_handler))
+      |> maybe_put_key(:guardian_key, Keyword.get(opts, :key))
+    end
+
+    def put_key(conn, key),
+      do: put_private(conn, :guardian_key, key)
+
+    def put_module(conn, module),
+      do: put_private(conn, :guardian_module, module)
+
+    def put_error_handler(conn, module),
+      do: put_private(conn, :guardian_error_handler, module)
+
+    def current_key(conn), do: conn.private[:guardian_key]
+
+    def current_module(conn), do: conn.private[:guardian_module]
+
+    def current_error_handler(conn), do: conn.private[:guardian_error_handler]
+
+    def fetch_key(conn, opts),
+      do: Keyword.get(opts, :key) || current_key(conn) || GPlug.default_key()
+
+    def fetch_module(conn, opts),
+      do: Keyword.get(opts, :module) || current_module(conn)
+
+    def fetch_module!(conn, opts) do
+      module = fetch_module(conn, opts)
+      if module do
+        module
+      else
+        raise "Module not in pipeline"
       end
+    end
 
-      defp put_modules(conn, opts) do
-        pipeline_opts = [
-          module: config(opts, :module),
-          error_handler: config(opts, :error_handler),
-          key: config(opts, :key, GPlug.default_key()),
-        ]
+    def fetch_error_handler(conn, opts),
+      do: Keyword.get(opts, :error_handler) || current_error_handler(conn)
 
-        Pipeline.call(conn, pipeline_opts)
+    def fetch_error_handler!(conn, opts) do
+      module = fetch_error_handler(conn, opts)
+      if module do
+        module
+      else
+        raise "Error Handler not in pipeline"
       end
-
-      defp raise_error(key),
-        do: raise "Config `#{key}` is missing for #{__MODULE__}"
     end
+
+    defp maybe_put_key(conn, _, nil), do: conn
+    defp maybe_put_key(conn, key, v), do: put_private(conn, key, v)
   end
-
-  def init(opts), do: opts
-
-  def call(conn, opts) do
-    conn
-    |> maybe_put_key(:guardian_module, Keyword.get(opts, :module))
-    |> maybe_put_key(:guardian_error_handler, Keyword.get(opts, :error_handler))
-    |> maybe_put_key(:guardian_key, Keyword.get(opts, :key))
-  end
-
-  def put_key(conn, key),
-    do: put_private(conn, :guardian_key, key)
-
-  def put_module(conn, module),
-    do: put_private(conn, :guardian_module, module)
-
-  def put_error_handler(conn, module),
-    do: put_private(conn, :guardian_error_handler, module)
-
-  def current_key(conn), do: conn.private[:guardian_key]
-
-  def current_module(conn), do: conn.private[:guardian_module]
-
-  def current_error_handler(conn), do: conn.private[:guardian_error_handler]
-
-  def fetch_key(conn, opts),
-    do: Keyword.get(opts, :key) || current_key(conn) || GPlug.default_key()
-
-  def fetch_module(conn, opts),
-    do: Keyword.get(opts, :module) || current_module(conn)
-
-  def fetch_module!(conn, opts) do
-    module = fetch_module(conn, opts)
-    if module do
-      module
-    else
-      raise "Module not in pipeline"
-    end
-  end
-
-  def fetch_error_handler(conn, opts),
-    do: Keyword.get(opts, :error_handler) || current_error_handler(conn)
-
-  def fetch_error_handler!(conn, opts) do
-    module = fetch_error_handler(conn, opts)
-    if module do
-      module
-    else
-      raise "Error Handler not in pipeline"
-    end
-  end
-
-  defp maybe_put_key(conn, _, nil), do: conn
-  defp maybe_put_key(conn, key, v), do: put_private(conn, key, v)
 end
--- lib/guardian/plug/verify_header.ex
+++ lib/guardian/plug/verify_header.ex
@@ -1,118 +1,120 @@
-defmodule Guardian.Plug.VerifyHeader do
-  @moduledoc """
+if Code.ensure_loaded?(Plug) do
+  defmodule Guardian.Plug.VerifyHeader do
+    @moduledoc """
 
-  Looks for and validates a token found in the `Authorization` header.
+    Looks for and validates a token found in the `Authorization` header.
 
-  In the case where:
+    In the case where:
 
-  a. The session is not loaded
-  b. A token is already found for `:key`
+    a. The session is not loaded
+    b. A token is already found for `:key`
 
-  This plug will not do anything.
+    This plug will not do anything.
 
-  This, like all other Guardian plugs, requires a Guardian pipleine to be setup.
-  It requires an implementation module, an error handler and a key.
+    This, like all other Guardian plugs, requires a Guardian pipleine to be setup.
+    It requires an implementation module, an error handler and a key.
 
-  These can be set either:
+    These can be set either:
 
-  1. Upstream on the connection with `plug Guardian.Pipeline`
-  2. Upstream on the connection with `Guardian.Pipeline.{put_module, put_error_handler, put_key}`
-  3. Inline with an option of `:module`, `:erorr_handler`, `:key`
+    1. Upstream on the connection with `plug Guardian.Pipeline`
+    2. Upstream on the connection with `Guardian.Pipeline.{put_module, put_error_handler, put_key}`
+    3. Inline with an option of `:module`, `:erorr_handler`, `:key`
 
-  If a token is found but is invalid, the error handler will be called with
-  `auth_error(conn, {:invalid_token, reason}, opts)`
+    If a token is found but is invalid, the error handler will be called with
+    `auth_error(conn, {:invalid_token, reason}, opts)`
 
-  Once a token has been found it will be decoded, the token and claims will be put onto the connection.
+    Once a token has been found it will be decoded, the token and claims will be put onto the connection.
 
-  They will be available using `Guardian.Plug.current_claims/2` and `Guardian.Plug.current_token/2`
+    They will be available using `Guardian.Plug.current_claims/2` and `Guardian.Plug.current_token/2`
 
-  Options:
+    Options:
 
-  * `claims` - The literal claims to check to ensure that a token is valid
-  * `realm` - The prefix for the token in the Authorization header.
-  * `key` - The location to store the information in the connection. Defaults to: `deafult`
+    * `claims` - The literal claims to check to ensure that a token is valid
+    * `realm` - The prefix for the token in the Authorization header.
+    * `key` - The location to store the information in the connection. Defaults to: `deafult`
 
-  ### Example
+    ### Example
 
-  ```elixir
+    ```elixir
 
-  # setup the upstream pipeline
+    # setup the upstream pipeline
 
-  plug Guardian.Plug.VerifyHeader, realm: "Bearer", claims: %{typ: "access"}
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer", claims: %{typ: "access"}
 
-  ```
+    ```
 
-  This will check the authorization header for a token
+    This will check the authorization header for a token
 
-  `Authorization Bearer: <token>`
+    `Authorization Bearer: <token>`
 
-  This token will be placed into the connection depending on the key and can be accessed with
-  `Guardian.Plug.current_token` and `Guardian.Plug.current_claims`
+    This token will be placed into the connection depending on the key and can be accessed with
+    `Guardian.Plug.current_token` and `Guardian.Plug.current_claims`
 
-  OR
+    OR
 
-  `MyApp.ImplementationModule.current_token` and `MyApp.ImplementationModule.current_claims`
-  """
+    `MyApp.ImplementationModule.current_token` and `MyApp.ImplementationModule.current_claims`
+    """
 
-  alias Guardian.Plug, as: GPlug
-  alias GPlug.Pipeline
+    alias Guardian.Plug, as: GPlug
+    alias GPlug.Pipeline
 
-  import Plug.Conn
+    import Plug.Conn
 
-  @spec init(Keyword.t) :: Keyword.t
-  def init(opts \\ []) do
-    realm = Keyword.get(opts, :realm)
+    @spec init(Keyword.t) :: Keyword.t
+    def init(opts \\ []) do
+      realm = Keyword.get(opts, :realm)
 
-    if realm do
-      {:ok, reg} = Regex.compile("#{realm}\:?\s+(.*)$", "i")
-      Keyword.put(opts, :realm_reg, reg)
-    else
-      opts
+      if realm do
+        {:ok, reg} = Regex.compile("#{realm}\:?\s+(.*)$", "i")
+        Keyword.put(opts, :realm_reg, reg)
+      else
+        opts
+      end
     end
-  end
 
-  @spec call(Plug.Conn.t, Keyword.t) :: Plug.Conn.t
-  def call(conn, opts) do
-    with nil <- GPlug.current_token(conn, opts),
-         {:ok, token} <- fetch_token_from_header(conn, opts),
-         module <- Pipeline.fetch_module!(conn, opts),
-         claims_to_check <- Keyword.get(opts, :claims, %{}),
-         key <- storage_key(conn, opts),
-         {:ok, claims} <- Guardian.decode_and_verify(module, token, claims_to_check, opts) do
+    @spec call(Plug.Conn.t, Keyword.t) :: Plug.Conn.t
+    def call(conn, opts) do
+      with nil <- GPlug.current_token(conn, opts),
+           {:ok, token} <- fetch_token_from_header(conn, opts),
+           module <- Pipeline.fetch_module!(conn, opts),
+           claims_to_check <- Keyword.get(opts, :claims, %{}),
+           key <- storage_key(conn, opts),
+           {:ok, claims} <- Guardian.decode_and_verify(module, token, claims_to_check, opts) do
 
-      conn
-      |> GPlug.put_current_token(token, key: key)
-      |> GPlug.put_current_claims(claims, key: key)
-    else
-      :no_token_found -> conn
-      {:error, reason} ->
         conn
-        |> Pipeline.fetch_error_handler!(opts)
-        |> apply(:auth_error, [conn, {:invalid_token, reason}, opts])
-      _ -> conn
+        |> GPlug.put_current_token(token, key: key)
+        |> GPlug.put_current_claims(claims, key: key)
+      else
+        :no_token_found -> conn
+        {:error, reason} ->
+          conn
+          |> Pipeline.fetch_error_handler!(opts)
+          |> apply(:auth_error, [conn, {:invalid_token, reason}, opts])
+        _ -> conn
+      end
     end
-  end
 
-  @spec fetch_token_from_header(Plug.Conn.t, Keyword.t) :: :no_token_found |
-                                                           {:ok, String.t}
-  defp fetch_token_from_header(conn, opts) do
-    headers = get_req_header(conn, "authorization")
-    fetch_token_from_header(conn, opts, headers)
-  end
-
-  @spec fetch_token_from_header(Plug.Conn.t, Keyword.t, Keyword.t) :: :no_token_found |
-                                                                      {:ok, String.t}
-  defp fetch_token_from_header(_, _, []), do: :no_token_found
-
-  defp fetch_token_from_header(conn, opts, [token|tail]) do
-    reg = Keyword.get(opts, :realm_reg, ~r/^(.*)$/)
-    trimmed_token = String.strip(token)
-    case Regex.run(reg, trimmed_token) do
-      [_, match] -> {:ok, String.strip(match)}
-      _ -> fetch_token_from_header(conn, opts, tail)
+    @spec fetch_token_from_header(Plug.Conn.t, Keyword.t) :: :no_token_found |
+                                                             {:ok, String.t}
+    defp fetch_token_from_header(conn, opts) do
+      headers = get_req_header(conn, "authorization")
+      fetch_token_from_header(conn, opts, headers)
     end
-  end
 
-  @spec storage_key(Plug.Conn.t, Keyword.t) :: String.t
-  defp storage_key(conn, opts), do: Pipeline.fetch_key(conn, opts)
+    @spec fetch_token_from_header(Plug.Conn.t, Keyword.t, Keyword.t) :: :no_token_found |
+                                                                        {:ok, String.t}
+    defp fetch_token_from_header(_, _, []), do: :no_token_found
+
+    defp fetch_token_from_header(conn, opts, [token|tail]) do
+      reg = Keyword.get(opts, :realm_reg, ~r/^(.*)$/)
+      trimmed_token = String.strip(token)
+      case Regex.run(reg, trimmed_token) do
+        [_, match] -> {:ok, String.strip(match)}
+        _ -> fetch_token_from_header(conn, opts, tail)
+      end
+    end
+
+    @spec storage_key(Plug.Conn.t, Keyword.t) :: String.t
+    defp storage_key(conn, opts), do: Pipeline.fetch_key(conn, opts)
+  end
 end
