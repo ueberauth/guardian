dc55119d6f6c84009d79f711a1f6129617f09975
Sean Callan <seancallan@gmail.com>
2017-07-30 10:29:14 -0600

    Additional refactors (#365)
    * Remove nested if and leverage pattern matching in function def
    
    * Remove unnecessary brackets and simplify function

--- lib/guardian/token/jwt/verify.ex
+++ lib/guardian/token/jwt/verify.ex
@@ -3,47 +3,38 @@ defmodule Guardian.Token.Jwt.Verify do
   Verifies standard jwt fields
   """
   use Guardian.Token.Verify
-  alias Guardian.Token.{Verify}
+
+  alias Guardian.Token.Verify
 
   @doc false
   def verify_claim(mod, "iss", %{"iss" => iss} = claims, _opts) do
     issuer = apply(mod, :config, [:issuer])
     verify_issuer = apply(mod, :config, [:verify_issuer])
 
-    if verify_issuer do
-      if issuer == iss do
-        {:ok, claims}
-      else
-        {:error, :invalid_issuer}
-      end
-    else
-      {:ok, claims}
+    cond do
+      verify_issuer && issuer == iss -> {:ok, claims}
+      verify_issuer -> {:error, :invalid_issuer}
+      true -> {:ok, claims}
     end
   end
 
   @doc false
+  def verify_claim(_mod, "nbf", %{"nbf" => nil} = claims, _opts), do: {:ok, claims}
   def verify_claim(mod, "nbf", %{"nbf" => nbf} = claims, _opts) do
-    if nbf == nil do
+    if Verify.time_within_drift?(mod, nbf) || nbf <= Guardian.timestamp() do
       {:ok, claims}
     else
-      if Verify.time_within_drift?(mod, nbf) || nbf <= Guardian.timestamp() do
-        {:ok, claims}
-      else
-        {:error, :token_not_yet_valid}
-      end
+      {:error, :token_not_yet_valid}
     end
   end
 
   @doc false
+  def verify_claim(_mod, "exp", %{"exp" => nil} = claims, _opts), do: {:ok, claims}
   def verify_claim(mod, "exp", %{"exp" => exp} = claims, _opts) do
-    if exp == nil do
+    if Verify.time_within_drift?(mod, exp) || exp >= Guardian.timestamp() do
       {:ok, claims}
     else
-      if Verify.time_within_drift?(mod, exp) || exp >= Guardian.timestamp() do
-        {:ok, claims}
-      else
-        {:error, :token_expired}
-      end
+      {:error, :token_expired}
     end
   end
 
