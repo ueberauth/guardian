73f3141cea13a64ff796ae15ebb9b50c8a1e23be
Daniel Neighman <has.sox@gmail.com>
2017-09-18 09:41:03 -0700

    Bumps the version of the beta (#393)
    We removed the extraneous error handler file

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Guardian.Mixfile do
   @moduledoc false
   use Mix.Project
 
-  @version "1.0.0-beta.0"
+  @version "1.0.0-beta.1"
   @url "https://github.com/ueberauth/guardian"
   @maintainers [
     "Daniel Neighman",
