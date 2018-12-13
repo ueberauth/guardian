b77198875787787748e748843733613b9059a7ab
Paul Zhang <paul.z.zhang@outlook.com>
2018-12-14 00:43:58 +0800

    support 1.4 (#533)

--- mix.exs
+++ mix.exs
@@ -159,7 +159,7 @@ defmodule Guardian.Mixfile do
       {:jose, "~> 1.8"},
 
       # Optional dependencies
-      {:phoenix, "~> 1.0 or ~> 1.2 or ~> 1.3", optional: true},
+      {:phoenix, "~> 1.3", optional: true},
       {:plug, "~> 1.3.3 or ~> 1.4", optional: true},
 
       # Tools
