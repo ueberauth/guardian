4779931a34de31797c0275c5dc1293ffa9dd2aa8
Hans <hanspagh@gmail.com>
2019-02-15 17:27:36 +0100

    update ex_doc to be able to generate docs

--- mix.exs
+++ mix.exs
@@ -165,7 +165,7 @@ defmodule Guardian.Mixfile do
       # Tools
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, ">= 1.0.0-rc4", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.19.1", only: [:dev], runtime: false},
       {:excoveralls, ">= 0.0.0", only: [:test], runtime: false},
       {:inch_ex, ">= 0.0.0", only: [:dev], runtime: false},
       {:jason, "~> 1.1", only: [:dev, :test], runtime: false}
