52ede257baaae7ee4d8eea408f4e56d2a89b01ac
Hans <hanspagh@gmail.com>
2019-04-15 10:36:11 +0200

    fix behavoir types

--- lib/guardian/permissions/permission_encoding.ex
+++ lib/guardian/permissions/permission_encoding.ex
@@ -3,6 +3,6 @@ defmodule Guardian.Permissions.PermissionEncoding do
   Behavior for the permission encoding
   """
 
-  @callback encode(List.t() | Integer.t(), String.t(), Map.t()) :: term
-  @callback decode(List.t() | Integer.t(), String.t(), Map.t()) :: term
+  @callback encode(list() | integer(), String.t(), map()) :: term
+  @callback decode(list() | integer(), String.t(), map()) :: term
 end
