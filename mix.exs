0d16e49d4a30d2f3362776292ddee2e2a2f0e371
Hans <hanspagh@gmail.com>
2019-12-20 16:38:55 +0100

    Update CHANGELOG.md (#631)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Guardian.Mixfile do
   @moduledoc false
   use Mix.Project
 
-  @version "2.0.0"
+  @version "2.1.0"
   @url "https://github.com/ueberauth/guardian"
   @maintainers [
     "Daniel Neighman",
